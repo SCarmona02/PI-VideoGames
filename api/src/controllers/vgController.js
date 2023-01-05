f76f6acfc055fc0d0d7194bc3e2fe1651dbcdb21
SCarmona02 <santiago.car2003@hotmail.com>
2023-01-05 00:47:53 -0500

    first commit

--- api/src/controllers/vgController.js
+++ api/src/controllers/vgController.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const axios = require("axios");
 const { API_KEY } = process.env;
-const {Videogame, Genre} = require("../db");
+const { Videogame, Genre } = require("../db");
 
 const gamesApi = "https://api.rawg.io/api/games";
 
@@ -13,14 +13,14 @@ module.exports = {
         let arrayApi = [];
         let necessary = [];
 
-        let responseFromDB = await Videogame.findAll({include: Genre,});
+        let responseFromDB = await Videogame.findAll({ include: Genre, });
         let responseFromAPI = await axios.get(`${gamesApi}?key=${API_KEY}`);
 
         arrayApi = [...responseFromAPI.data.results];
         let nextApi = responseFromAPI.data.next;
         necessary = [...responseFromDB];
 
-        for (let i = 0; i < 4; i++){
+        for (let i = 0; i < 4; i++) {
             let responseFromNextApi = await axios.get(nextApi);
             arrayApi = [...arrayApi, ...responseFromNextApi.data.results]
             nextApi = responseFromNextApi.data.next;
@@ -28,9 +28,9 @@ module.exports = {
 
         arrayApi.map((game) => {
 
-            function findGenre(){
+            function findGenre() {
                 let resultGenre = [];
-                game.genres.map((genre) => resultGenre.push({"id": genre.id, "name": genre.name.toLowerCase() }));
+                game.genres.map((genre) => resultGenre.push({ "id": genre.id, "name": genre.name.toLowerCase() }));
                 return resultGenre;
             }
 
@@ -54,14 +54,14 @@ module.exports = {
         }
 
         return necessary;
-        
+
     },
 
     detailGame: async function (id) {
         let games = await this.listGames();
         let arrayDetail = [];
         let gameFilter = [];
-        if (id.length > 6){
+        if (id.length > 6) {
 
         } else {
             id = parseInt(id);
@@ -69,60 +69,65 @@ module.exports = {
 
         gameFilter = games.filter(game => game.id === id);
 
-        if (gameFilter.length){
-            let responseFromAPI = await axios.get(`${gamesApi}/${id}?key=${API_KEY}`);
-            let data = responseFromAPI.data;
+        if (gameFilter.length) {
+            if (id.length <= 6) {
+                let responseFromAPI = await axios.get(`${gamesApi}/${id}?key=${API_KEY}`);
+                let data = responseFromAPI.data;
 
-            function findGenre(){
-                let resultGenre = [];
-                data.genres.map((genre) => resultGenre.push({"id": genre.id, "name": genre.name }));
-                return resultGenre;
+                function findGenre() {
+                    let resultGenre = [];
+                    data.genres.map((genre) => resultGenre.push({ "id": genre.id, "name": genre.name }));
+                    return resultGenre;
+                }
+
+                function findPlatform() {
+                    let resultPlat = [];
+                    data.platforms.map((platform) => resultPlat.push({ "id": platform.platform.id, "name": platform.platform.name }));
+                    return resultPlat;
+                }
+
+                let detail = {
+                    "id": data.id,
+                    "name": data.name,
+                    "description": data.description,
+                    "released": data.released,
+                    "rating": data.rating,
+                    "platforms": findPlatform(),
+                    "image": data.background_image,
+                    "genres": findGenre()
+                }
+                arrayDetail.push(detail);
+
+                return arrayDetail;
             }
-
-            function findPlatform(){
-                let resultPlat = [];
-                data.platforms.map((platform) => resultPlat.push({ "id": platform.platform.id, "name": platform.platform.name }));
-                return resultPlat;
+            else {
+                return gameFilter;
             }
-
-            let detail = {
-                "id": data.id,
-                "name": data.name,
-                "description": data.description,
-                "released": data.released,
-                "rating": data.rating,
-                "platforms": findPlatform(),
-                "image": data.background_image,
-                "genres": findGenre()
-            }
-            arrayDetail.push(detail);
-
-            return arrayDetail;
         } else {
             throw new Error(`Searched VideoGame not found with id ${id}`);
         }
     },
 
-    newGame: async function(body){
+    newGame: async function (body) {
         const { name, description, released, rating, platforms, image, genres } = body;
 
         body.rating ? body.rating = parseFloat(rating) : body.rating = 0.0;
 
-        if (image.length === 0){
+        if (image.length === 0) {
             body.image = "https://www.sopitas.com/wp-content/uploads/2017/08/controles-videojuegos.jpg";
         }
 
-        if(!name || !description || !platforms){
+        if (!name || !description || !platforms) {
             throw new Error('Name, description and platforms are required')
         } else {
             const newVideoGame = await Videogame.create(body);
-            if(genres){
+            if (genres) {
                 genres.forEach(async genre => {
                     let responseFromDB = await Genre.findAll();
                     responseFromDB.find(genero => genero.name == genre ? newVideoGame.addGenres(genero.id) : false)
                 })
             }
-            return ("New game registred!")
+            return ("New game registred!");
         }
     }
 
--- client/src/components/Form/Form.jsx
+++ client/src/components/Form/Form.jsx
@@ -4,12 +4,42 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useHistory } from "react-router-dom";
 import { getAllVideoGames, getAllGenres, getVideoGameQuery, setError, setGames, createVideoGame } from "../../redux/actions/actions";
 
+const validate = (input) => {
+    let errors = {};
+    if(input.name.length < 3){
+        errors.name = "*The videogame's name is required (3 letters min)"
+    }
+
+    if(input.description.length < 3){
+        errors.description = "Description is required (3 letters min)"
+    }
+
+    if(input.platforms.length < 1){
+        errors.platforms = "Platforms is required (1 platform min)"
+    }
+
+    if(parseInt(input.released) < 1950 ||  parseInt(input.released) > 2024){
+        errors.released = "Date must be between 1950 - 2024"
+    }
+
+    if(parseFloat(input.rating) < 0.01 || parseFloat(input.rating) > 5){
+        errors.rating = "Rating must be between 0.01 - 5"
+    }
+    
+    if(input.image.length > 0 && !(/\S+\.\S+/).test(input.image)) {
+        errors.image = "Image must be URL"
+    }
+
+    return errors;
+}
+
 const Form = () => {
 
     const dispatch = useDispatch();
     const history = useHistory();
     const findGame = useSelector(state => state.videoGames);
     const [loading, setLoading] = useState("");
+    const [platform, setPlatform] = useState("")
     const genres = useSelector(state => state.genres);
 
     const [input, setInput] = useState({
@@ -52,6 +82,11 @@ const Form = () => {
             [event.target.name]: event.target.value
         })
 
+        setErrors(validate({
+            ...input,
+            [event.target.name]: event.target.value
+        }))
+
         if(event.target.name === "name"){
             await setLoading("Loading");
             await dispatch(setGames());
@@ -71,13 +106,23 @@ const Form = () => {
             }
         }
         if(event.target.name === "platforms"){
-            if(!input.platforms.includes(event.target.value)){
+            event.preventDefault();
+            if(!input.platforms.includes(platform)){
                 setInput({
                     ...input,
-                    platforms: [...input.platforms, event.target.value]
+                    platforms: [...input.platforms, platform]
                 })
+                setPlatform("")
+                setErrors(validate({
+                    ...input,
+                    platforms: [...input.platforms, platform]
+                }))
             }
         }
+    }
+
+    const handlePlatform = (event) => {
+        setPlatform(event.target.value)
     }
 
     const handleDeleteGenre = (event) => {
@@ -96,7 +141,13 @@ const Form = () => {
             ...input,
             platforms: newPlatforms
         })
+        setErrors(validate({
+            ...input,
+            platforms: newPlatforms
+        }))
     }
+
+    let buttonDisabled = !(input.name.length) || !(input.description.length) || !(input.platforms.length) || (errors.name || errors.description || errors.released || errors.rating || errors.platforms || errors.image)
 
     return (
         <div>
@@ -109,15 +160,63 @@ const Form = () => {
                     <input type="text" value={input.name} name="name" autoComplete="off" onChange={(event) => changeHandler(event)} placeholder="Name"></input>
                     {loading === "Loading" && (<p>Cargando...</p>)}
                     {findGame.length && input.name.length ? (<p>This videogame already exist</p>) : false}
+                    {errors.name && (<p>{errors.name}</p>)}
                 </div>
                 <div>
                     <label htmlFor="description">Description: <span>*</span></label>
                     <textarea value={input.description} name="description" autoComplete="off" onChange={(event) => changeHandler(event)} placeholder="Description"></textarea>
+                    {errors.description && (<p>{errors.description}</p>)}
                 </div>
                 <div>
                     <label htmlFor="released">Released: </label>
                     <input type="date" value={input.released} name="released" min="1950-01-01" max="2024-01-01" autoComplete="off" onChange={(event) => changeHandler(event)}></input>
+                    {errors.released && (<p>{errors.released}</p>)}
                 </div>
+                <div>
+                    <label htmlFor="rating">Rating: </label>
+                    <input type="number" step="0.01" min="0.01" max="5" value={input.rating} name="rating" autoComplete="off" onChange={(event) => changeHandler(event)} placeholder="0,01 - 5,0"></input>
+                    {errors.rating && (<p>{errors.rating}</p>)}
+                </div>
+                <div>
+                    <label htmlFor="platforms">Platforms: <span>*</span></label>
+                    <input type="text" value={platform} onChange={(event) => handlePlatform(event)} autoComplete="off" placeholder="Platform"></input>
+                    {errors.platforms && (<p>{errors.platforms}</p>)}
+                    <button name="platforms" onClick={(event) => handleSelect(event)}>+</button>
+                    {input.platforms?.map((platform, index) => {
+                        return (
+                            <div key={index}>
+                                <span key={platform}>{platform}</span>
+                                <button name={platform} onClick={(event) => handleDeletePlatform(event)}>X</button>
+                            </div>
+                        )
+                    })}
+                </div>
+                <div>
+                    <label htmlFor="image">Image: </label>
+                    <input type="url" value={input.image} name="image" autoComplete="off" onChange={(event) => changeHandler(event)} placeholder="URL"></input>
+                    {errors.image && (<p>{errors.image}</p>)}
+                </div>
+                <div>
+                    <label htmlFor="genres">Genres: </label>
+                    <select name="genres" onChange={(event) => handleSelect(event)} defaultValue="title">
+                        <option value="title" disabled name="Genres">Genres</option>
+                        {genres.map(genre => {
+                            return (
+                                <option value={genre.name} key={genre.id}>{genre.name[0].toUpperCase() + genre.name.slice(1)}</option>
+                            )
+                        })}
+                    </select>
+                    {input.genres?.map((genre, index) => {
+                        return (
+                            <div key={index}>
+                                <span key={genre}>{genre[0].toUpperCase() + genre.slice(1)}</span>
+                                <button name={genre} onClick={(event) => handleDeleteGenre(event)}>X</button>
+                            </div>
+                        )
+                    })}
+                </div>
+
+                <button disabled={buttonDisabled} type="submit">Save</button>
             </form>
         </div>
     )
